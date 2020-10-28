6841742dc97eda4bf5c29cbe6f153714b2409d91
Robert Neagu <67923421+RobertNeaguQC@users.noreply.github.com>
2020-10-28 14:05:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+/*
+<div class="card-body">
+<p class="card-text">Completely beautiful website and amazing support! This is my second website from this author and I love both of the sites so much and she has helped me so well when I needed it!</p>
+<img src="./avatar1.png" class="" alt="...">
+</div>
+<h5 class="card-title">Card title</h5>
+<p>science.com</p>
+</div>
+ <img class="card-img-top img-fluid" src="//placehold.it/500x280" alt="Card image cap">*/
